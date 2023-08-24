5fc0c2a3481360ceadfbc7afb2562b2003a53c1e
Aija Repsa <120313261+AijaRe@users.noreply.github.com>
2023-08-24 20:27:25 +0100

    updates h file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -4,12 +4,14 @@
 /* malloc, free, exit */
 #include <stdlib.h>
 #include <limits.h>
+#include <stdbool.h>
 #include "../libft/libft.h"
 
 typedef struct s_stack{
 	int val;
-	int pos;
-	size_t size;
+	int index;
+	//size_t size;
+	bool has_index;
 	struct s_stack *next;
 } t_stack;
 
@@ -27,13 +29,16 @@ int	ft_intcmp(int a, int b);
 long	ft_atol(char *str);
 t_stack	*last_node(t_stack *top);
 t_stack	*second_last_node(t_stack *top);
-void	push_new_val(t_stack **top, int val);
-t_stack	*find_highest(t_stack *stack);
-int	check_sorted(t_stack *stack);
+t_stack	*get_max(t_stack *stack);
+t_stack	*get_min(t_stack *stack);
+bool	check_if_ascending(t_stack *stack);
+bool	check_if_descending(t_stack *stack);
+int	ft_list_size(t_stack *stack);
 /* operations.c */
 void	node_swap(t_stack **top);
 void	rotate(t_stack **top);
 void	rev_rotate(t_stack **top);
+//void	push_new_val(t_stack **top, int val);
 void	push_to(t_stack **top_1, t_stack **top_2, char list);
 void	move(void (*operation)(t_stack**), t_stack **stack, char list);
 void	ss(t_stack **stack_a, t_stack **stack_b);
